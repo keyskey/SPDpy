05a9c42605697e43bac9770d9f687723813a992a
keyskey <39644776+keyskey@users.noreply.github.com>
2018-06-15 16:35:58 +0900

    modify comment

--- heatmap.py
+++ heatmap.py
@@ -1,40 +1,42 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from  fastSPD import Battle
-   
+ 
+num_episode = 10    # Number of simulation episodes
+
 fig = plt.figure()
 
-"""    
-for i in range(1, Battle.num_ens+1):
-    df = pd.read_csv('output{}.csv'.format(i)) 
-    df_pivot = df.pivot('Dg','Dr', 'Fraction of Cooperation')
+# If you have a result of many episodes
+if num_episode > 1:
+    for i in range(1, num_episode+1):
+        df = pd.read_csv('output{}.csv'.format(i)) 
+        df_pivot = df.pivot('Dg','Dr', 'Fraction of Cooperation')
       
-    ax = fig.add_subplot(2, 5, i)
-    sns.heatmap(df_pivot, 
-                vmin = 0.0, vmax = 1.0, 
-                annot = False, 
-                cbar = False,
-                cmap="jet",
-                square = False)
+        ax = fig.add_subplot(2, 5, i)
+        sns.heatmap(df_pivot, 
+                    vmin = 0.0, vmax = 1.0, 
+                    annot = False, 
+                    cbar = False,
+                    cmap="jet",
+                    square = False)
           
     plt.gca().invert_yaxis()
     ax.set_title("Episode{}".format(i))
+    plt.tight_layout()
+    fig.suptitle('Fraction of Cooperation', fontsize=16)
 
-plt.tight_layout()
-fig.suptitle('Fraction of Cooperation', fontsize=16)
-#plt.subplots_adjust(top=1.0)
-"""
+# If you have a result of one episodes
+else:
+    df = pd.read_csv('output1.csv') 
+    df_pivot = df.pivot('Dg','Dr', 'Fraction of Cooperation')
+    sns.heatmap(df_pivot, 
+                vmin = 0.0, vmax = 1.0, 
+                annot = True, 
+                cbar = True,
+                cmap="jet",
+                square = False)
+    plt.gca().invert_yaxis()
+    plt.title('Fraction of Cooperation') 
+    plt.show()
 
-df = pd.read_csv('output1.csv') 
-df_pivot = df.pivot('Dg','Dr', 'Fraction of Cooperation')
-sns.heatmap(df_pivot, 
-            vmin = 0.0, vmax = 1.0, 
-            annot = True, 
-            cbar = True,
-            cmap="jet",
-            square = False)
-plt.gca().invert_yaxis()
-plt.title('Fraction of Cooperation') 
-plt.show()
-#fig.savefig('Fc.png')
+fig.savefig('Dg-Dr-Diagram.png')
