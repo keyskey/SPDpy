0c5e056a9fa11efd4a8b8960aa5f530330ddd21c
Kei <39644776+keyskey@users.noreply.github.com>
2018-11-20 21:56:31 +0900

    Change column name

--- time_evolution.py
+++ time_evolution.py
@@ -7,7 +7,7 @@ ax = plt.subplot(1,1,1)
 
 for episode in range(1, 101):
     df = pd.read_csv(f'time_evolution_episode{episode}.csv')
-    ax.scatter(df['time'], df['Fraction of Cooperation'], s = 15, label = f'episode{episode}')    
+    ax.scatter(df['time'], df['Fc'], s = 15, label = f'episode{episode}')    
     ax.set_ylim([0, 1.0])
     ax.set_xscale("log")
     ax.set_xticks([1,10,100,1000])
